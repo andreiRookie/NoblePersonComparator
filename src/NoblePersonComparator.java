ce7d00c500cd50df8b7b846539c6742ae138ddf1
AndreiRookie <andrey.idem@gmail.com>
2023-01-31 18:20:53 +0400

    maxWordsInSurname added

--- src/NoblePersonComparator.java
+++ src/NoblePersonComparator.java
@@ -2,13 +2,22 @@ import java.util.Comparator;
 
 public class NoblePersonComparator implements Comparator<Person> {
 
+    private int maxWordsInSurname;
+    public NoblePersonComparator(int maxWordsInSurname) {
+        this.maxWordsInSurname = maxWordsInSurname;
+    }
     @Override
     public int compare(Person o1, Person o2) {
-        String[] o1SurnameWords = o1.getSurname().split(" ");
-        String[] o2SurnameWords = o2.getSurname().split(" ");
+        String[] o1SurnameWords = o1.getSurname().split("-");
+        String[] o2SurnameWords = o2.getSurname().split("-");
 
-        if (o1SurnameWords.length != o2SurnameWords.length) {
-            return o1SurnameWords.length - o2SurnameWords.length;
+        int o1Count = o1SurnameWords.length;
+        int o2Count = o2SurnameWords.length;
+
+        if (o1Count >= maxWordsInSurname && o2Count >= maxWordsInSurname) {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        } else if (o1Count != o2Count) {
+            return o1Count - o2Count;
         } else {
             return Integer.compare(o1.getAge(), o2.getAge());
         }
